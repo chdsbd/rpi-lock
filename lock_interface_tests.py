728a3335e64af0dcb4aac2d2cfaac60a02afe12c
Christopher Dignam <me@christopher.xyz>
2015-10-19 16:55:47 -0400

    Added python 3 support and added configuration file

--- lock_interface_tests.py
+++ lock_interface_tests.py
@@ -32,13 +32,13 @@ class LockInterfaceTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert rv.status_code == 302
         rv = self.login('admin', 'default')
-        assert 'Add User' in rv.data
+        assert 'Add User' in rv.data.decode("utf-8")
 
     def test_logout(self):
         rv = self.login('admin', 'default')
-        assert 'Add User' in rv.data
+        assert 'Add User' in rv.data.decode("utf-8")
         rv = self.logout()
-        assert 'You were logged out' in rv.data
+        assert 'You were logged out' in rv.data.decode("utf-8")
 
     def test_404(self):
         rv = self.app.get('/random_url')
@@ -48,11 +48,11 @@ class LockInterfaceTestCase(unittest.TestCase):
         rv = self.app.get('/login')
         assert rv.status_code == 200
         rv = self.login('badusername', lock_interface.PASSWORD)
-        assert 'Invalid username' in rv.data
+        assert 'Invalid username' in rv.data.decode("utf-8")
         rv = self.login(lock_interface.USERNAME, 'badpassword')
-        assert 'Invalid password' in rv.data
+        assert 'Invalid password' in rv.data.decode("utf-8")
         rv = self.login(lock_interface.USERNAME, lock_interface.PASSWORD)
-        assert 'You were logged in' in rv.data
+        assert 'You were logged in' in rv.data.decode("utf-8")
 
     def test_adding_entry(self):
         self.login(lock_interface.USERNAME, lock_interface.PASSWORD)
@@ -61,34 +61,34 @@ class LockInterfaceTestCase(unittest.TestCase):
             note='test node',
             binary='00000010010101'))
         rv = self.app.get('/')
-        assert 'New entry was successfully posted' in rv.data
+        assert 'New entry was successfully posted' in rv.data.decode("utf-8")
         self.app.post('/add', data=dict(
             name='Joe Shmoe',
             note='test node',
             binary='not binary data'))
         rv = self.app.get('/')
-        assert 'is not binary' in rv.data
+        assert 'is not binary' in rv.data.decode("utf-8")
         self.app.post('/delete', data=dict(
             user_id=1))
         rv = self.app.get('/')
-        assert 'User deleted successfully.' in rv.data
+        assert 'User deleted successfully.' in rv.data.decode("utf-8")
         self.app.post('/add', data=dict(
             name='',
             note='',
             binary=''))
         rv = self.app.get('/')
         assert ('name is below min value' and
-                'binary is below min value') in rv.data
+                'binary is below min value') in rv.data.decode("utf-8")
 
     def test_status_page(self):
         self.login(lock_interface.USERNAME, lock_interface.PASSWORD)
         rv = self.app.get('/status')
-        assert 'Status Page' in rv.data
+        assert 'Status Page' in rv.data.decode("utf-8")
 
     def test_log_page(self):
         self.login(lock_interface.USERNAME, lock_interface.PASSWORD)
         rv = self.app.get('/log')
-        assert 'Door Lock Access Log' in rv.data
+        assert 'Door Lock Access Log' in rv.data.decode("utf-8")
 
 if __name__ == '__main__':
     unittest.main()
